81508870c540fda64eccc0f4d3e6e9c7d2a26fbd
benjamingoldenage <80452692+benjamingoldenlineage@users.noreply.github.com>
2021-10-07 23:02:55 +0300

    07 oct v2

---
+++ coding-challanges/cc-005-vote-count/vote-count.py
@@ -0,0 +1,18 @@
+# majority_vote(["A", "A", "A", "B", "C", "A"])
+
+# - Expected Outputs:
+
+# ```text
+# Input: majority_vote(["A", "A", "A", "B", "C", "A"]) 
+# Output: "A"
+
+# CC solution:
+# majority_vote(["A", "A", "A", "B", "C", "A"])
+lst = ["A", "A", "A", "B", "C", "A"]
+def majority_vote(lst):
+    for i in lst:
+        if lst.count(i) > len(lst)/2:
+            return i
+
+print (len(lst))
+
